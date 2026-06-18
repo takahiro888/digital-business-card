b50df958f16328701b0b282d43ed9d87a17347ce
takahiro888 <takahiro.45666@gmail.com>
2026-06-19 00:09:06 +0900

    refac:　不要コード削除

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -8,34 +8,6 @@ export type UserRow = {
   created_at: string;
 };
 
-// export class UserRow {
-//   public id: number;
-//   public name: string;
-//   public description: string;
-//   public github_id: string;
-//   public qiita_id: string;
-//   public x_id: string;
-//   public created_at: string | null;
-
-//   constructor(
-//     id: number,
-//     name: string,
-//     description: string,
-//     github_id: string,
-//     qiita_id: string,
-//     x_id: string,
-//     created_at: string | null,
-//   ) {
-//     this.id = id;
-//     this.name = name;
-//     this.description = description;
-//     this.github_id = github_id;
-//     this.qiita_id = qiita_id;
-//     this.x_id = x_id;
-//     this.created_at = created_at;
-//   }
-// }
-
 export type UserSkillRow = {
   id: number;
   user_id: string;
